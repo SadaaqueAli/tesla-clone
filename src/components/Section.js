2527b7559733e360fec88fa0563350eb48fea018
SadaaqueAli <sadaaqueali@gmail.com>
2024-10-04 00:50:32 +0500

    first component

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,8 +4,23 @@ import styled from 'styled-components'
 function Section() {
   return (
     <Wrap>
-      Section
-    </Wrap>
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchiess Delivery</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>
+            Custom Order
+          </LeftButton>
+
+          <RightButton>
+            Exiting Inventory
+          </RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/Down-Arrow.png" />
+      </Buttons>
+    </Wrap >
   )
 }
 
@@ -14,5 +29,65 @@ export default Section
 const Wrap = styled.div`
 width:100vw;
 height:100vh;
-background:orange;
+background-size:cover;
+background-position:center;
+background-repeat:no-repeat;
+background-image: url('/images/model-s.jpg');
+display: flex;
+flex-direction:column;
+justify-content: space-between;
+align-items: center;
+
+`
+
+const ItemText = styled.div`
+padding-top:15vh;
+text-align:center;
+`
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom:30px;
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin:8px;
+  
+`;
+
+const RightButton = styled.div`
+  background-color: white;
+  height: 40px;
+  width: 256px;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.65;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin:8px;
+`;
+
+const DownArrow = styled.img`
+height:40px;
+overflow-x:hidden;
+animation: animateDown infinite 1.5s;
+`
+
+const Buttons = styled.div`
 `
