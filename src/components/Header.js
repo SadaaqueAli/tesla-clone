ddd680c6c62a53cd53f99f3384e8dd7de89ec73f
SadaaqueAli <sadaaqueali@gmail.com>
2024-10-05 00:47:37 +0500

    section added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="" />
+      </a>
+    </Container>
   )
 }
 
 export default Header
+
+const Container = styled.div`
+min-height: 60px;
+
+`
