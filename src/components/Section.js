ea105cb40ffec84074e02d6324ca1a90c802e188
SadaaqueAli <sadaaqueali@gmail.com>
2024-10-12 09:00:51 +0500

    ADD: redux dfor cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
 
 function Section({ title, description, leftBtnText, rightBtnText, backGroundImg }) {
   return (
     <Wrap bgImage = {backGroundImg}>
-      <ItemText>
+      <Fade bottom>
+        <ItemText>
         <h1>{title}</h1>
         <p>{description}</p>
       </ItemText>
+      </Fade>
       <Buttons>
+      <Fade bottom>
         <ButtonGroup>
           <LeftButton>
             {leftBtnText}
@@ -18,6 +22,7 @@ function Section({ title, description, leftBtnText, rightBtnText, backGroundImg 
           </RightButton>
           }
         </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/Down-Arrow.png" />
       </Buttons>
     </Wrap >
